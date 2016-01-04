ba425dba2c5561c21c75bd83d3cdcaa78920a5fe
Franziska Müller <franziska7993@t-online.de>
2016-01-04 14:01:04 +0100

    add noise fix

--- denoising_grad_ascent.m
+++ denoising_grad_ascent.m
@@ -1,17 +1,23 @@
 function T = denoising_grad_ascent(N, sigma, eta)
 iter = 2000;
+postCurve = zeros(iter+1,1);
 T = N;
+postCurve(1) = denoising_lp(N, N, sigma);
 for i = 1:iter
     v = denoising_lp(T, N, sigma);
     g = denoising_grad_llh(T, N, sigma) + mrf_grad_log_gaussian_prior(T, sigma);
     T2 = T + eta * g;
     v2 = denoising_lp(T2, N, sigma);
+    postCurve(iter+1) = v2;
     if v2 > v
         T = T2;
     else
+        display(iter);
         break;
     end
 end
+
+plot(postCurve(:,1));
 
 end
 
